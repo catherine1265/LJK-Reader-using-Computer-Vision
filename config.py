0ef50398783a97db0beb91c7608ef230a3e155c0
Kezia Gracella Natanaella Haryono <keziaharyono17@gmail.com>
2026-06-08 09:21:15 +0700

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ ALL_ROIS = {
     'NAMA_MATA_KULIAH': (540, 545, 940, 625),
     'KODE_KELAS': (540, 658, 740, 725),
     'RUANGAN': (810, 658, 960, 725),
-    'NO_MEJA': (810, 755, 960, 860),
+    'NO_MEJA': (810, 755, 960, 890),
 }
 
 HANDWRITING_ROIS = [
